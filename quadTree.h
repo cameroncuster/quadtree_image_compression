b4701e136167b8d69d03fd8a6e8951b41c0bccd4
cameroncuster <custer.cameron@gmail.com>
2021-03-25 16:00:57 -0600

    documentation in quadTree include udpated

--- quadTree.h
+++ quadTree.h
@@ -35,47 +35,65 @@ class QuadTree
 	unsigned leafNodeCount;
 
 	public:
+	// construct a QuadTree given an image and a threshold
 	QuadTree( byte **&gray, const unsigned width, const unsigned height,
 			const byte thresh );
-	~QuadTree( );
 
-	void decrementThreshold( byte **&gray, const unsigned width,
-			const unsigned height );
+	// extract the compressed image from the QuadTree
+	void getCompressedImage( byte **&gray ) const;
+
+	// draw the quadrant boundry lines on the compressed image
+	void drawLines( byte **&gray ) const;
+
 	void incrementThreshold( byte **&gray, const unsigned width,
 			const unsigned height );
 
-	unsigned leafCount( ) const;
-
-	void getCompressedImage( byte **&gray ) const;
-
-	void drawLines( byte **&gray ) const;
+	void decrementThreshold( byte **&gray, const unsigned width,
+			const unsigned height );
 
 	unsigned size( ) const;
 
+	unsigned leafCount( ) const;
+
 	byte threshold( ) const;
 
+	~QuadTree( );
+
 	private:
+	// subdivide the image into Quadrants and store them as nodes in the QuadTree
 	node *subdivide( byte **&gray, pair<unsigned, unsigned> topLeft,
 			pair<unsigned, unsigned> bottomRight );
 
+	// determine if a quadrant must be further subdivided and return the mean of
+	// the quadrant if necessary
 	byte evalSubdivision( byte **&gray,
 			const pair<unsigned, unsigned> topLeft,
 			const pair<unsigned, unsigned> bottomRight, bool need ) const;
 
-	void insert( byte **&gray, node *quadrant );
-	void remove( byte **&gray, node *quadrant );
-
+	// construct an image from the QuadTree
 	void buildCompressedImage( byte **&gray, const node *quadrant ) const;
+
+	// add the lines to the QuadTree
 	void addLines( byte **&gray, const node *quadrant ) const;
 
+	// insert nodes into the QuadTree an account of an decreased threshold
+	void insert( byte **&gray, node *quadrant );
+
+	// remove nodes the QuadTree on account of an increased threshold
+	void remove( byte **&gray, node *quadrant );
+
+	// return a vector containing the boundry points of all four children of the
+	// quadrant
 	vector<pair<unsigned, unsigned>> calculateChildBoundryPoints(
 			pair<unsigned, unsigned> topLeft,
 			pair<unsigned, unsigned> bottomRight ) const;
 
+	// return the center of a quadrant given the topLeft and bottomRight points
+	// of a quadrant
 	pair<unsigned, unsigned> center( pair<unsigned, unsigned> topLeft,
 			pair<unsigned, unsigned> bottomRight ) const;
 
-
+	// recursively delete all nodes in the subtree of node ( n )
 	void clear( node *n );
 };
 #endif
