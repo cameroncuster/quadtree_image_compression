aefb64d0fab834a5e5a3423e52b1481fbc3d1400
cameroncuster <custer.cameron@gmail.com>
2021-03-26 11:52:48 -0600

    set expandtab vimrc with retab for spacing

--- quadTree.cpp
+++ quadTree.cpp
@@ -18,116 +18,116 @@ using namespace std;
 /// subdivide the image to construct the QuadTree node structure recursively ///
 ////////////////////////////////////////////////////////////////////////////////
 QuadTree::node *QuadTree::subdivide( byte **&gray,
-		pair<unsigned, unsigned> topLeft, pair<unsigned, unsigned> bottomRight )
+        pair<unsigned, unsigned> topLeft, pair<unsigned, unsigned> bottomRight )
 {
-	vector<pair<unsigned, unsigned>> childBoundryPoints =
-		calculateChildBoundryPoints( topLeft, bottomRight );
+    vector<pair<unsigned, unsigned>> childBoundryPoints =
+        calculateChildBoundryPoints( topLeft, bottomRight );
 
-	node *quadrant = new node( topLeft, bottomRight,
-			evalSubdivision( gray, topLeft, bottomRight, 0 ) );
-	nodeCount++;
+    node *quadrant = new node( topLeft, bottomRight,
+            evalSubdivision( gray, topLeft, bottomRight, 0 ) );
+    nodeCount++;
 
-	if( !evalSubdivision( gray, topLeft, bottomRight, 1 ) ||
-			topLeft.first - bottomRight.first < 2 ||
-			topLeft.second - bottomRight.second < 2 )
-	{
-		leafNodeCount++;
-		return quadrant;
-	}
+    if( !evalSubdivision( gray, topLeft, bottomRight, 1 ) ||
+            topLeft.first - bottomRight.first < 2 ||
+            topLeft.second - bottomRight.second < 2 )
+    {
+        leafNodeCount++;
+        return quadrant;
+    }
 
-	quadrant->nw = subdivide( gray, childBoundryPoints[0], childBoundryPoints[1] );
-	quadrant->sw = subdivide( gray, childBoundryPoints[2], childBoundryPoints[3] );
-	quadrant->ne = subdivide( gray, childBoundryPoints[4], childBoundryPoints[5] );
-	quadrant->se = subdivide( gray, childBoundryPoints[6], childBoundryPoints[7] );
+    quadrant->nw = subdivide( gray, childBoundryPoints[0], childBoundryPoints[1] );
+    quadrant->sw = subdivide( gray, childBoundryPoints[2], childBoundryPoints[3] );
+    quadrant->ne = subdivide( gray, childBoundryPoints[4], childBoundryPoints[5] );
+    quadrant->se = subdivide( gray, childBoundryPoints[6], childBoundryPoints[7] );
 
-	return quadrant;
+    return quadrant;
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-/// behaves as a need subdivide and function while also averaging the		 ///
-/// pixelVales in a given quadrant for a performance optomization			 ///
+/// behaves as a need subdivide and function while also averaging the        ///
+/// pixelVales in a given quadrant for a performance optomization            ///
 /// the function behavior is relaint on the C++ implicit boolean casting of  ///
-/// integer values because when the mean is returned the mean is alwalys	 ///
+/// integer values because when the mean is returned the mean is alwalys     ///
 /// positive unless the mean is 0 in which case the quadrant does not need to///
 /// be subdivided by any threshold becuase every pixel in the quadrant can be///
-/// represented by a single value											 ///
+/// represented by a single value                                            ///
 ////////////////////////////////////////////////////////////////////////////////
 byte QuadTree::evalSubdivision( byte **&gray,
-		const pair<unsigned, unsigned> topLeft,
-		const pair<unsigned, unsigned> bottomRight, bool need ) const
+        const pair<unsigned, unsigned> topLeft,
+        const pair<unsigned, unsigned> bottomRight, bool need ) const
 {
-	unsigned i, j;
-	unsigned sum = 0;
-	byte mean;
-	byte mx = gray[ topLeft.second ][ topLeft.first ];
-	byte mn = gray[ topLeft.second ][ topLeft.first ];
-	if( bottomRight.second - topLeft.second == 0 ||
-			bottomRight.first - topLeft.first == 0 )
-		return 0;
-	for( i = topLeft.second; i < bottomRight.second; i++ )
-		for( j = topLeft.first; j < bottomRight.first; j++ )
-		{
-			sum += gray[i][j];
-			if( gray[i][j] > mx )
-				mx = gray[i][j];
-			if( gray[i][j] < mn )
-				mn = gray[i][j];
-		}
-	mean = sum / ( ( bottomRight.second - topLeft.second ) *
-			( bottomRight.first - topLeft.first ) ) + 0.5;
-	if( need && mx - mean <= tolerance && mean - mn <= tolerance )
-		return 0;
-	return mean;
+    unsigned i, j;
+    unsigned sum = 0;
+    byte mean;
+    byte mx = gray[ topLeft.second ][ topLeft.first ];
+    byte mn = gray[ topLeft.second ][ topLeft.first ];
+    if( bottomRight.second - topLeft.second == 0 ||
+            bottomRight.first - topLeft.first == 0 )
+        return 0;
+    for( i = topLeft.second; i < bottomRight.second; i++ )
+        for( j = topLeft.first; j < bottomRight.first; j++ )
+        {
+            sum += gray[i][j];
+            if( gray[i][j] > mx )
+                mx = gray[i][j];
+            if( gray[i][j] < mn )
+                mn = gray[i][j];
+        }
+    mean = sum / ( ( bottomRight.second - topLeft.second ) *
+            ( bottomRight.first - topLeft.first ) ) + 0.5;
+    if( need && mx - mean <= tolerance && mean - mn <= tolerance )
+        return 0;
+    return mean;
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///			construct the compressed image from the QuadTree object			 ///
+///         construct the compressed image from the QuadTree object          ///
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::buildCompressedImage( byte **&gray, const node *quadrant ) const
 {
-	unsigned i, j;
+    unsigned i, j;
 
-	if( quadrant->isLeaf( ) )
-	{
-		for( i = quadrant->topLeft.second; i < quadrant->bottomRight.second; i++ )
-			for( j = quadrant->topLeft.first; j < quadrant->bottomRight.first; j++ )
-				gray[i][j] = quadrant->pixelValue;
-		return;
-	}
+    if( quadrant->isLeaf( ) )
+    {
+        for( i = quadrant->topLeft.second; i < quadrant->bottomRight.second; i++ )
+            for( j = quadrant->topLeft.first; j < quadrant->bottomRight.first; j++ )
+                gray[i][j] = quadrant->pixelValue;
+        return;
+    }
 
-	buildCompressedImage( gray, quadrant->nw );
-	buildCompressedImage( gray, quadrant->ne );
-	buildCompressedImage( gray, quadrant->sw );
-	buildCompressedImage( gray, quadrant->se );
+    buildCompressedImage( gray, quadrant->nw );
+    buildCompressedImage( gray, quadrant->ne );
+    buildCompressedImage( gray, quadrant->sw );
+    buildCompressedImage( gray, quadrant->se );
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///		draw the lines as directed by the dimenstions stored in the leaves	 ///
-///		node																 ///
+///     draw the lines as directed by the dimenstions stored in the leaves   ///
+///     node                                                                 ///
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::addLines( byte **&gray, const node *quadrant ) const
 {
-	unsigned i;
+    unsigned i;
 
-	if( quadrant->isLeaf( ) )
-	{
-		for( i = quadrant->topLeft.second; i < quadrant->bottomRight.second; i++ )
-			gray[i][ quadrant->topLeft.first ] = 255;
-		for( i = quadrant->topLeft.first; i < quadrant->bottomRight.first; i++ )
-			gray[ quadrant->topLeft.second ][i] = 255;
-		return;
-	}
+    if( quadrant->isLeaf( ) )
+    {
+        for( i = quadrant->topLeft.second; i < quadrant->bottomRight.second; i++ )
+            gray[i][ quadrant->topLeft.first ] = 255;
+        for( i = quadrant->topLeft.first; i < quadrant->bottomRight.first; i++ )
+            gray[ quadrant->topLeft.second ][i] = 255;
+        return;
+    }
 
-	addLines( gray, quadrant->nw );
-	addLines( gray, quadrant->ne );
-	addLines( gray, quadrant->sw );
-	addLines( gray, quadrant->se );
+    addLines( gray, quadrant->nw );
+    addLines( gray, quadrant->ne );
+    addLines( gray, quadrant->sw );
+    addLines( gray, quadrant->se );
 }
 
 
@@ -137,103 +137,103 @@ void QuadTree::addLines( byte **&gray, const node *quadrant ) const
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::insert( byte **&gray, node *quadrant )
 {
-	vector<pair<unsigned, unsigned>> childBoundryPoints =
-		calculateChildBoundryPoints( quadrant->topLeft, quadrant->bottomRight );
+    vector<pair<unsigned, unsigned>> childBoundryPoints =
+        calculateChildBoundryPoints( quadrant->topLeft, quadrant->bottomRight );
 
-	if( quadrant->isLeaf( ) )
-	{
-		if( evalSubdivision( gray, quadrant->topLeft, quadrant->bottomRight, 1 ) )
-		{
-			quadrant->nw = subdivide( gray, childBoundryPoints[0], childBoundryPoints[1] );
-			quadrant->sw = subdivide( gray, childBoundryPoints[2], childBoundryPoints[3] );
-			quadrant->ne = subdivide( gray, childBoundryPoints[4], childBoundryPoints[5] );
-			quadrant->se = subdivide( gray, childBoundryPoints[6], childBoundryPoints[7] );
-			leafNodeCount--;
-		}
-		return;
-	}
+    if( quadrant->isLeaf( ) )
+    {
+        if( evalSubdivision( gray, quadrant->topLeft, quadrant->bottomRight, 1 ) )
+        {
+            quadrant->nw = subdivide( gray, childBoundryPoints[0], childBoundryPoints[1] );
+            quadrant->sw = subdivide( gray, childBoundryPoints[2], childBoundryPoints[3] );
+            quadrant->ne = subdivide( gray, childBoundryPoints[4], childBoundryPoints[5] );
+            quadrant->se = subdivide( gray, childBoundryPoints[6], childBoundryPoints[7] );
+            leafNodeCount--;
+        }
+        return;
+    }
 
-	insert( gray, quadrant->nw );
-	insert( gray, quadrant->sw );
-	insert( gray, quadrant->ne );
-	insert( gray, quadrant->se );
+    insert( gray, quadrant->nw );
+    insert( gray, quadrant->sw );
+    insert( gray, quadrant->ne );
+    insert( gray, quadrant->se );
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///		  updates QuadTree on account of new threshold by removing nodes	 ///
+///       updates QuadTree on account of new threshold by removing nodes     ///
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::remove( byte **&gray, node *quadrant )
 {
-	if( !evalSubdivision( gray, quadrant->topLeft, quadrant->bottomRight, 1 ) )
-	{
-		clear( quadrant->nw );
-		clear( quadrant->sw );
-		clear( quadrant->ne );
-		clear( quadrant->se );
+    if( !evalSubdivision( gray, quadrant->topLeft, quadrant->bottomRight, 1 ) )
+    {
+        clear( quadrant->nw );
+        clear( quadrant->sw );
+        clear( quadrant->ne );
+        clear( quadrant->se );
 
-		quadrant->nw = nullptr;
-		quadrant->sw = nullptr;
-		quadrant->ne = nullptr;
-		quadrant->se = nullptr;
+        quadrant->nw = nullptr;
+        quadrant->sw = nullptr;
+        quadrant->ne = nullptr;
+        quadrant->se = nullptr;
 
-		return;
-	}
+        return;
+    }
 
-	remove( gray, quadrant->nw );
-	remove( gray, quadrant->sw );
-	remove( gray, quadrant->ne );
-	remove( gray, quadrant->se );
+    remove( gray, quadrant->nw );
+    remove( gray, quadrant->sw );
+    remove( gray, quadrant->ne );
+    remove( gray, quadrant->se );
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///				  return the number of leaves in the subTree				 ///
+///               return the number of leaves in the subTree                 ///
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::subTreeLeafNodeCount( node *quadrant, unsigned &newLeafNodeCount ) const
 {
-	if( quadrant->isLeaf( ) )
-	{
-		newLeafNodeCount++;
-		return;
-	}
+    if( quadrant->isLeaf( ) )
+    {
+        newLeafNodeCount++;
+        return;
+    }
 
-	subTreeLeafNodeCount( quadrant->nw, newLeafNodeCount );
-	subTreeLeafNodeCount( quadrant->sw, newLeafNodeCount );
-	subTreeLeafNodeCount( quadrant->ne, newLeafNodeCount );
-	subTreeLeafNodeCount( quadrant->se, newLeafNodeCount );
+    subTreeLeafNodeCount( quadrant->nw, newLeafNodeCount );
+    subTreeLeafNodeCount( quadrant->sw, newLeafNodeCount );
+    subTreeLeafNodeCount( quadrant->ne, newLeafNodeCount );
+    subTreeLeafNodeCount( quadrant->se, newLeafNodeCount );
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
 /// returns the top-left and bottom-right points of all 4 children quadrants ///
-/// given the top-left and bottom-right of the parent						 ///
+/// given the top-left and bottom-right of the parent                        ///
 ////////////////////////////////////////////////////////////////////////////////
 vector<pair<unsigned, unsigned>> QuadTree::calculateChildBoundryPoints(
-		pair<unsigned, unsigned> topLeft,
-		pair<unsigned, unsigned> bottomRight ) const
+        pair<unsigned, unsigned> topLeft,
+        pair<unsigned, unsigned> bottomRight ) const
 {
-	pair<unsigned, unsigned> c = center( topLeft, bottomRight );
+    pair<unsigned, unsigned> c = center( topLeft, bottomRight );
 
-	return { topLeft, c, { topLeft.first, c.second },
-		{ c.first, bottomRight.second }, { c.first, topLeft.second },
-		{ bottomRight.first, c.second }, c, bottomRight };
+    return { topLeft, c, { topLeft.first, c.second },
+        { c.first, bottomRight.second }, { c.first, topLeft.second },
+        { bottomRight.first, c.second }, c, bottomRight };
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
 /// returns the center of a given quadrant with respect to the top-left and  ///
-///							  bottom-right points							 ///
+///                           bottom-right points                            ///
 ////////////////////////////////////////////////////////////////////////////////
 pair<unsigned, unsigned> QuadTree::center( pair<unsigned, unsigned> topLeft,
-		pair<unsigned, unsigned> bottomRight ) const
+        pair<unsigned, unsigned> bottomRight ) const
 {
-	return { topLeft.first + ( bottomRight.first - topLeft.first ) / 2,
-		topLeft.second + ( bottomRight.second - topLeft.second ) / 2 };
+    return { topLeft.first + ( bottomRight.first - topLeft.first ) / 2,
+        topLeft.second + ( bottomRight.second - topLeft.second ) / 2 };
 }
 
 
@@ -243,120 +243,120 @@ pair<unsigned, unsigned> QuadTree::center( pair<unsigned, unsigned> topLeft,
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::clear( node *n )
 {
-	if( n == nullptr )
-	{
-		nodeCount--;
-		delete n;
-		return;
-	}
+    if( n == nullptr )
+    {
+        nodeCount--;
+        delete n;
+        return;
+    }
 
-	clear( n->nw );
-	clear( n->sw );
-	clear( n->ne );
-	clear( n->se );
+    clear( n->nw );
+    clear( n->sw );
+    clear( n->ne );
+    clear( n->se );
 }
 
 
 
 /*   ____        _     _ _        ___       _             __
-	|  _ \ _   _| |__ | (_) ___  |_ _|_ __ | |_ ___ _ __ / _| __ _  ___ ___
-	| |_) | | | | '_ \| | |/ __|  | || '_ \| __/ _ \ '__| |_ / _` |/ __/ _ \
-	|  __/| |_| | |_) | | | (__   | || | | | ||  __/ |  |  _| (_| | (_|  __/
-	|_|    \__,_|_.__/|_|_|\___| |___|_| |_|\__\___|_|  |_|  \__,_|\___\___|
+    |  _ \ _   _| |__ | (_) ___  |_ _|_ __ | |_ ___ _ __ / _| __ _  ___ ___
+    | |_) | | | | '_ \| | |/ __|  | || '_ \| __/ _ \ '__| |_ / _` |/ __/ _ \
+    |  __/| |_| | |_) | | | (__   | || | | | ||  __/ |  |  _| (_| | (_|  __/
+    |_|    \__,_|_.__/|_|_|\___| |___|_| |_|\__\___|_|  |_|  \__,_|\___\___|
  */
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///			Construct a new QuadTree given an image and a threshold			 ///
-///	throws an exception of value 1 given an invalid image, width, or height  ///
+///         Construct a new QuadTree given an image and a threshold          ///
+/// throws an exception of value 1 given an invalid image, width, or height  ///
 ////////////////////////////////////////////////////////////////////////////////
 QuadTree::QuadTree( byte **&gray, const unsigned width, const unsigned height,
-		const byte thresh ) : tolerance( thresh ), nodeCount( 0 ), leafNodeCount( 0 )
+        const byte thresh ) : tolerance( thresh ), nodeCount( 0 ), leafNodeCount( 0 )
 {
-	if( gray == nullptr || !width || !height )
-		throw 1;
-	pair<unsigned, unsigned> tl = { 0, 0 };
-	pair<unsigned, unsigned> br = { width, height };
-	root = subdivide( gray, tl, br );
+    if( gray == nullptr || !width || !height )
+        throw 1;
+    pair<unsigned, unsigned> tl = { 0, 0 };
+    pair<unsigned, unsigned> br = { width, height };
+    root = subdivide( gray, tl, br );
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///			return the compressed image constructed from the QuadTree		 ///
+///         return the compressed image constructed from the QuadTree        ///
 ////////////////////////////////////////////////////////////////////////////////
 byte **QuadTree::getCompressedImage( ) const
 {
-	byte **gray = alloc2D_byte( root->bottomRight.first, root->bottomRight.second );
-	buildCompressedImage( gray, root );
-	return gray;
+    byte **gray = alloc2D_byte( root->bottomRight.first, root->bottomRight.second );
+    buildCompressedImage( gray, root );
+    return gray;
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///						draw every line in the QuadTree						 ///
+///                     draw every line in the QuadTree                      ///
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::drawLines( byte **&gray ) const
 {
-	addLines( gray, root );
+    addLines( gray, root );
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///		   delete nodes while possible to maintain the looser threshold		 ///
+///        delete nodes while possible to maintain the looser threshold      ///
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::incrementThreshold( byte **&gray )
 {
-	unsigned newLeafNodeCount = 0;
-	if( tolerance > 254 )
-		return;
-	tolerance++;
-	remove( gray, root );
-	subTreeLeafNodeCount( root, newLeafNodeCount );
-	leafNodeCount = newLeafNodeCount;
+    unsigned newLeafNodeCount = 0;
+    if( tolerance > 254 )
+        return;
+    tolerance++;
+    remove( gray, root );
+    subTreeLeafNodeCount( root, newLeafNodeCount );
+    leafNodeCount = newLeafNodeCount;
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-///		   insert nodes as necessary to maintain the tighter threshold		 ///
+///        insert nodes as necessary to maintain the tighter threshold       ///
 ////////////////////////////////////////////////////////////////////////////////
 void QuadTree::decrementThreshold( byte **&gray )
 {
-	if( tolerance < 1 )
-		return;
-	tolerance--;
-	insert( gray, root );
+    if( tolerance < 1 )
+        return;
+    tolerance--;
+    insert( gray, root );
 }
 
 
 
 unsigned QuadTree::size( ) const
 {
-	return nodeCount;
+    return nodeCount;
 }
 
 
 
 unsigned QuadTree::leafCount( ) const
 {
-	return leafNodeCount;
+    return leafNodeCount;
 }
 
 
 
 byte QuadTree::threshold( ) const
 {
-	return tolerance;
+    return tolerance;
 }
 
 
 
 QuadTree::~QuadTree( )
 {
-	clear( root );
-	delete root;
+    clear( root );
+    delete root;
 }
--- quadTree.h
+++ quadTree.h
@@ -9,92 +9,92 @@ typedef unsigned char byte;
 
 class QuadTree
 {
-	struct node
-	{
-		pair<unsigned, unsigned> topLeft;
-		pair<unsigned, unsigned> bottomRight;
-		byte pixelValue;
-		node* nw;
-		node* sw;
-		node* ne;
-		node* se;
+    struct node
+    {
+        pair<unsigned, unsigned> topLeft;
+        pair<unsigned, unsigned> bottomRight;
+        byte pixelValue;
+        node* nw;
+        node* sw;
+        node* ne;
+        node* se;
 
-		node( pair<unsigned, unsigned> tl, pair<unsigned, unsigned> br, byte pixVal,
-				node *northwest = nullptr, node *southwest = nullptr,
-				node *northeast = nullptr, node *southeast = nullptr ):
-			topLeft( tl ), bottomRight( br ), pixelValue( pixVal ),
-			nw( northwest ), sw( southwest ), ne( northeast ), se( southeast ) { }
+        node( pair<unsigned, unsigned> tl, pair<unsigned, unsigned> br, byte pixVal,
+                node *northwest = nullptr, node *southwest = nullptr,
+                node *northeast = nullptr, node *southeast = nullptr ):
+            topLeft( tl ), bottomRight( br ), pixelValue( pixVal ),
+            nw( northwest ), sw( southwest ), ne( northeast ), se( southeast ) { }
 
-		bool isLeaf( ) const;
-	};
+        bool isLeaf( ) const;
+    };
 
-	node *root;
+    node *root;
 
-	byte tolerance;
-	unsigned nodeCount;
-	unsigned leafNodeCount;
+    byte tolerance;
+    unsigned nodeCount;
+    unsigned leafNodeCount;
 
 public:
-	// construct a QuadTree given an image and a threshold
-	QuadTree( byte **&gray, const unsigned width, const unsigned height,
-			const byte thresh );
+    // construct a QuadTree given an image and a threshold
+    QuadTree( byte **&gray, const unsigned width, const unsigned height,
+            const byte thresh );
 
-	// extract the compressed image from the QuadTree
-	byte **getCompressedImage( ) const;
+    // extract the compressed image from the QuadTree
+    byte **getCompressedImage( ) const;
 
-	// draw the quadrant boundry lines on the compressed image
-	void drawLines( byte **&gray ) const;
+    // draw the quadrant boundry lines on the compressed image
+    void drawLines( byte **&gray ) const;
 
-	void incrementThreshold( byte **&gray );
+    void incrementThreshold( byte **&gray );
 
-	void decrementThreshold( byte **&gray );
+    void decrementThreshold( byte **&gray );
 
-	unsigned size( ) const;
+    unsigned size( ) const;
 
-	unsigned leafCount( ) const;
+    unsigned leafCount( ) const;
 
-	byte threshold( ) const;
+    byte threshold( ) const;
 
-	~QuadTree( );
+    ~QuadTree( );
 
 private:
-	// subdivide the image into Quadrants and store them as nodes in the QuadTree
-	node *subdivide( byte **&gray, pair<unsigned, unsigned> topLeft,
-			pair<unsigned, unsigned> bottomRight );
+    // subdivide the image into Quadrants and store them as nodes in the QuadTree
+    node *subdivide( byte **&gray, pair<unsigned, unsigned> topLeft,
+            pair<unsigned, unsigned> bottomRight );
 
-	// determine if a quadrant must be further subdivided and return the mean of
-	// the quadrant if necessary
-	byte evalSubdivision( byte **&gray,
-			const pair<unsigned, unsigned> topLeft,
-			const pair<unsigned, unsigned> bottomRight, bool need ) const;
+    // determine if a quadrant must be further subdivided and return the mean of
+    // the quadrant if necessary
+    byte evalSubdivision( byte **&gray,
+            const pair<unsigned, unsigned> topLeft,
+            const pair<unsigned, unsigned> bottomRight, bool need ) const;
 
-	// construct an image from the QuadTree
-	void buildCompressedImage( byte **&gray, const node *quadrant ) const;
+    // construct an image from the QuadTree
+    void buildCompressedImage( byte **&gray, const node *quadrant ) const;
 
-	// add the lines to the QuadTree
-	void addLines( byte **&gray, const node *quadrant ) const;
+    // add the lines to the QuadTree
+    void addLines( byte **&gray, const node *quadrant ) const;
 
-	// insert nodes into the QuadTree an account of an decreased threshold
-	void insert( byte **&gray, node *quadrant );
+    // insert nodes into the QuadTree an account of an decreased threshold
+    void insert( byte **&gray, node *quadrant );
 
-	// remove nodes the QuadTree on account of an increased threshold
-	void remove( byte **&gray, node *quadrant );
+    // remove nodes the QuadTree on account of an increased threshold
+    void remove( byte **&gray, node *quadrant );
 
-	// evaluate the number of leaf nodes in the subtree ( quadrant )
-	void subTreeLeafNodeCount( node *quadrant, unsigned &leafNodeCount ) const;
+    // evaluate the number of leaf nodes in the subtree ( quadrant )
+    void subTreeLeafNodeCount( node *quadrant, unsigned &leafNodeCount ) const;
 
-	// return a vector containing the boundry points of all four children of the
-	// quadrant
-	vector<pair<unsigned, unsigned>> calculateChildBoundryPoints(
-			pair<unsigned, unsigned> topLeft,
-			pair<unsigned, unsigned> bottomRight ) const;
+    // return a vector containing the boundry points of all four children of the
+    // quadrant
+    vector<pair<unsigned, unsigned>> calculateChildBoundryPoints(
+            pair<unsigned, unsigned> topLeft,
+            pair<unsigned, unsigned> bottomRight ) const;
 
-	// return the center of a quadrant given the topLeft and bottomRight points
-	// of a quadrant
-	pair<unsigned, unsigned> center( pair<unsigned, unsigned> topLeft,
-			pair<unsigned, unsigned> bottomRight ) const;
+    // return the center of a quadrant given the topLeft and bottomRight points
+    // of a quadrant
+    pair<unsigned, unsigned> center( pair<unsigned, unsigned> topLeft,
+            pair<unsigned, unsigned> bottomRight ) const;
 
-	// recursively delete all nodes in the subtree of node ( n )
-	void clear( node *n );
+    // recursively delete all nodes in the subtree of node ( n )
+    void clear( node *n );
 };
 #endif
